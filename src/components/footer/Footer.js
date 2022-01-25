6fb30fbde4af5c81d3a957f059526579bef510cd
Prakash Shaw <prakashshawin2050@gmail.com>
2022-01-25 18:03:25 +0530

    second

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Footer.css'
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="copyright">
+        Made 
+        {/* with{' '}
+        <span role="img" aria-label="thinking">
+          ❤️
+        </span>{' '} */}
+        by Prakash
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
